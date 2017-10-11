33cce0103f5a20cf9dd881b87724e16d6dd2eb0d
arizz96 <arizz96@gmail.com>
2017-10-11 15:13:50 +0200

    Fixed constants and added Procfile

--- config/constants.js
+++ config/constants.js
@@ -1,12 +1,12 @@
 module.exports = {
-        'project_name':'express-mvc-generator',
-  'adminname': 'Rajaram',
-  'adminmail': 'test@gmail.com',
+  'project_name':'remind-o-matic',
+  'adminname': 'The Bitowls',
+  'adminmail': 'thebitowls@gmail.com',
   'host':"http://localhost:8042",
   'email_smtp_host':'Your Email SMTP',
   'email_smtp_port':'YOUR SMTP PORT',
   "smtp_from_eamil":"from@email.com",
-  "smtp_from_name":"express-mvc-generator",
-  "alert_email":"aler@email.com",
+  "smtp_from_name":"remind-o-matic",
+  "alert_email":"thebitowls@gmail.com",
   "alert_email_name":"Aler Name",
 };
