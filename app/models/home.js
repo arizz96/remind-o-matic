aff57c52f239d637bab57fb77c61052781039a9b
arizz96 <arizz96@gmail.com>
2017-10-11 15:46:32 +0200

    Code style review

--- app/models/home.js
+++ app/models/home.js
@@ -1,11 +1,10 @@
-
 //app/models/user.js
 //load the things we need
 var mongoose = require('mongoose');
 var bcrypt   = require('bcrypt-nodejs');
 
 //define the schema for our user model
-var userSchema = mongoose.Schema({  
+var userSchema = mongoose.Schema({
   _id:{ type: Number, default: 1 },
   name: String,
   mail: String,
@@ -17,16 +16,15 @@ var userSchema = mongoose.Schema({
   role_id: { type: Number, default: 2 }
 });
 
-
 //methods ======================
 //generating a hash
 userSchema.methods.generateHash = function(password) {
- return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
 //checking if password is valid
 userSchema.methods.validPassword = function(password) {
- return bcrypt.compareSync(password, this.password);
+  return bcrypt.compareSync(password, this.password);
 };
 
 //create the model for users and expose it to our app
