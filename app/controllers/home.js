78dc1f17ac0af1633bcd36674fb9e41d7197395a
arizz96 <arizz96@gmail.com>
2017-10-19 10:31:44 +0200

    Completed welcome controller
    The welcome controller will now return basic controller’s data, such as
    action and status, and will respond with the translated welcome text

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,7 +2,11 @@ var numeral = require('numeral');
 var dateFormat = require('dateformat');
 
 exports.welcome = function(req, res) {
-  res.json({ body: 'Hey' });
+  res.json({
+    action: 'welcome',
+    status: 200,
+    body: req.__('welcome')
+  });
 }
 
 exports.ask = function(req, res) {
