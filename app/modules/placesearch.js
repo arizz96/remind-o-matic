44055a7d30b351c4f03586658aa0b3055c759447
pllgiulio <pllgiulio96@gmail.com>
2017-11-18 12:01:52 +0100

    Typo fix

--- app/modules/placesearch.js
+++ app/modules/placesearch.js
@@ -2,19 +2,18 @@ var rp = require('request-promise');
 
 nearbysearch = function(parameters, outputFormat){
   var url = "https://" + process.env.MAPS_HOST + process.env.MAPS_NEAR_PLACES_URL + outputFormat + "?";
-  for(var key in parameters){
+  for(var key in parameters)
     url += key + "=" + parameters[key] + "&";
-  }
   url = url.slice(0, -1);
   console.log(url);
 
   return rp(url)
   .then(function (repos) {
-      return repos
+      return repos;
   })
   .catch(function (err) {
       // API call failed...
-      console.log(err)
+      console.log(err);
   });
 };
 
