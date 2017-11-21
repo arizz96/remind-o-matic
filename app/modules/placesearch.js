210b3413fa38c1fc27748eefc10c9134e44b5669
arizz96 <arizz96@gmail.com>
2017-11-21 12:40:16 +0100

    Refactored place API search
    Now ask EP call Google API in order to provider nearby search of POIs.
    Refactored logics with promises.

--- app/modules/placesearch.js
+++ app/modules/placesearch.js
@@ -1,20 +1,44 @@
 var rp = require('request-promise');
 
-nearbysearch = function(parameters, outputFormat){
-  var url = "https://" + process.env.MAPS_HOST + process.env.MAPS_NEAR_PLACES_URL + outputFormat + "?";
-  for(var key in parameters)
-    url += key + "=" + parameters[key] + "&";
-  url = url.slice(0, -1);
-  console.log(url);
-
-  return rp(url)
-  .then(function (repos) {
-      return repos;
+search = function search(keyword, location, rankby = 'prominence') {
+  return maps.geocode({
+    address: location
+  }).asPromise()
+  .then(function(response){
+    var parameters = {};
+    parameters.location = response.json.results[0].geometry.location.lat + "," + response.json.results[0].geometry.location.lng;
+    parameters.key = process.env.MAPS_KEY;
+    parameters.rankby = rankby;
+    if(rankby == "prominence")
+      parameters.radius = process.env.SEARCH_RADIUS;
+    parameters.keyword = keyword;
+    return parameters;
   })
-  .catch(function (err) {
-      // API call failed...
-      console.log(err);
-  });
-};
+  .then(function(parameters){
+    var url = "https://" + process.env.MAPS_HOST + process.env.MAPS_NEAR_PLACES_URL + process.env.FORMAT + "?";
+    for(var key in parameters)
+      url += key + "=" + parameters[key] + "&";
+    url = url.slice(0, -1);
 
-exports.nearbysearch = nearbysearch;
+    return rp(url);
+  })
+  .then(function(repos) {
+    return repos;
+  })
+  .then(function(response){
+    // console.log(JSON.parse(response).results[0]);
+    jsonResponse = JSON.parse(response);
+    values = [];
+    for(i = 0; i < Math.min(jsonResponse.results.length, process.env.LIMIT); i++)
+      values.push({
+        lat:     jsonResponse.results[i].geometry.location.lat,
+        lng:     jsonResponse.results[i].geometry.location.lng,
+        icon:    jsonResponse.results[i].icon,
+        name:    jsonResponse.results[i].name,
+        address: jsonResponse.results[i].vicinity
+      });
+    return values;
+  });
+}
+
+exports.search = search;
